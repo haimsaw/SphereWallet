1726c39726b284c5e4122175e1b2e106ad10ab2b
Yaniv Franco <yafranco@microsoft.com>
2019-07-21 13:23:05 +0300

    intilize client

---
+++ Client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>{{"ZenGo - Microsoft "}}</span>
+        <span class="font-weight-light">Hackathon</span>
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-content></v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
