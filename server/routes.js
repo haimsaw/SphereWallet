eb3b5a31b5487c7bd5863ee7d1e3b51a38276ec1
Yaniv Franco <yafranco@microsoft.com>
2019-07-22 15:02:03 +0300

    Recive route add

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,22 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const bitcoin = require("bitcoinjs-lib");
+const TESTNET = bitcoin.networks.testnet;
 
 router.get("/", (req, res) => {
   res.json("Welcome To ShpereWallet Hackathon");
 });
-router.get("/genkey", (req, res) => res.send("ABCD"));
+router.post("/send", (req, res) => {
+  // Generate public key and adress
+});
 
-router.post("/sgn", (req, res) => res.send(""));
+router.get("/recieve", (req, res) => {
+  // Generate public key and address
+  const keyPair = bitcoin.ECPair.makeRandom();
+  const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey, network: TESTNET });
+
+  //return adress to client
+  res.json(address);
+});
 
 module.exports = router;
