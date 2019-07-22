71d7cb7f06fdf129eec7a96ea182fef769630225
Yaniv Franco <yafranco@microsoft.com>
2019-07-22 15:18:27 +0300

    recieve route

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 const bodyParser = require("body-parser");
-
 const express = require("express");
 const app = express();
 const port = 3000;
--- server/routes.js
+++ server/routes.js
@@ -2,21 +2,36 @@ const express = require("express");
 const router = express.Router();
 const bitcoin = require("bitcoinjs-lib");
 const TESTNET = bitcoin.networks.testnet;
+var db = {};
 
 router.get("/", (req, res) => {
   res.json("Welcome To ShpereWallet Hackathon");
 });
 router.post("/send", (req, res) => {
-  // Generate public key and adress
+  // Get key pair
+  let keyPair = getOrGenerateKeyPair();
 });
 
 router.get("/recieve", (req, res) => {
-  // Generate public key and address
-  const keyPair = bitcoin.ECPair.makeRandom();
+  // Generate key pair if not exists
+  let keyPair = getOrGenerateKeyPair();
+
+  // Create address
   const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey, network: TESTNET });
 
   //return adress to client
   res.json(address);
 });
+
+function getOrGenerateKeyPair() {
+  let keyPair;
+  if (!db.keyPair) {
+    keyPair = bitcoin.ECPair.makeRandom();
+    db.keyPair = keyPair;
+  } else {
+    keyPair = db.keyPair;
+  }
+  return keyPair;
+}
 
 module.exports = router;
